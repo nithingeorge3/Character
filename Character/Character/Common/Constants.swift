6aab6f9304d09b507b41982aef87f3be08b5b97f
Nithin George <nithingeorge3@gmail.com>
2021-03-01 11:11:38 +0000

    Updated UI

--- Character/Character/Common/Constants.swift
+++ Character/Character/Common/Constants.swift
@@ -5,10 +5,15 @@
 //  Created by Nitin George on 24/02/2021.
 //
 
-import Foundation
 import UIKit
 
-let sb: UIStoryboard?       = UIStoryboard(name: "Main", bundle: nil)
+//enum with implicit assignment
+enum Appearance: Int {
+    case season1 = 1, season2, season3, season4, season5, season6, season7, season8, season9, season10
+    func getID() -> Int {
+        return self.rawValue
+  }
+}
 
-let kFetchCharacterListURL          = "/characters"
-let kCharacterDetailImageHeight     = 288.0
+let sb: UIStoryboard?       = UIStoryboard(name: "Main", bundle: nil)
+let kFetchCharacterListURL  = "/characters"
